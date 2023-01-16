dfc2ffd5a0f3991d81281a7e976cec0ab0643151
LCVcode <lcvcode@gmail.com>
2023-01-16 12:43:17 -0700

    Initial commit

---
+++ matrix-tools/matrix_tools.go
@@ -0,0 +1,88 @@
+package matrix_tools
+
+type Matrix struct {
+    data [][]float64
+    rows int
+    cols int
+}
+
+func NewMatrix(data [][]float64) Matrix {
+    rows := len(data)
+    cols := len(data[0])
+    return Matrix{data: data, rows: rows, cols: cols}
+}
+
+func (m Matrix) Transpose() Matrix {
+    data := make([][]float64, m.cols)
+    for i := 0; i < m.cols; i++ {
+        data[i] = make([]float64, m.rows)
+        for j := 0; j < m.rows; j++ {
+            data[i][j] = m.data[j][i]
+        }
+    }
+    return Matrix{data: data, rows: m.cols, cols: m.rows}
+}
+
+func (m Matrix) EqualTo(n Matrix) bool {
+    if m.cols != n.cols || m.rows != n.rows {
+        return false
+    }
+    for i := 0; i < m.rows; i++ {
+        for j := 0; j < m.cols; j++ {
+            if m.data[i][j] != n.data[i][j] {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+func (m Matrix) Add(n Matrix) Matrix {
+    if m.cols != n.cols || m.rows != n.rows {
+        panic("matrices have different dimensions")
+    }
+
+    data := make([][]float64, m.rows)
+    for i := 0; i < m.rows; i++ {
+        data [i] = make([]float64, m.cols)
+        for j := 0; j < m.cols; j++ {
+            data[i][j] = m.data[i][j] + n.data[i][j]
+        }
+    }
+    return Matrix{data: data, rows: m.rows, cols: m.cols}
+}
+
+func (m Matrix) Multiply(n Matrix) Matrix {
+    if m.cols != n.rows {
+        panic("matrices have incompatible dimensions")
+    }
+
+    data := make([][]float64, m.rows)
+    for i := 0; i < m.rows; i++ {
+        data[i] = make([]float64, n.cols)
+        for j := 0; j < n.cols; j++ {
+            sum := 0.0
+            for k := 0; k < m.cols; k++ {
+                sum += m.data[i][k] * n.data[k][j]
+            }
+            data[i][j] = sum
+        }
+    }
+    return Matrix{data: data, rows: m.rows, cols: n.cols}
+}
+
+func (m Matrix) Dot(n Matrix) float64 {
+    if m.cols != 1 || n.rows != 1 {
+        panic("matrices should be vectors")
+    }
+
+    if m.rows != n.cols {
+        panic("vectors have different lengths")
+    }
+
+    sum := 0.0
+    for i := 0; i < m.rows; i++ {
+        sum += m.data[i][0] * n.data[0][i]
+    }
+    return sum
+}
---
+++ matrix-tools/matrix_tools_test.go
@@ -0,0 +1,127 @@
+package matrix_tools
+
+import "testing"
+
+func AssertEqualHelper(t *testing.T, expected, actual interface{}) {
+    if actual != expected {
+        t.Errorf("Expected %v, got %v", expected, actual)
+    }
+}
+
+func AssertPanic(t *testing.T) {
+    defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+}
+
+func TestTranspose(t *testing.T) {
+    m := NewMatrix([][]float64{{1, 2, 3}})
+    n := NewMatrix([][]float64{{1}, {2}, {3}})
+    result := m.EqualTo(n.Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = n.EqualTo(m.Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = n.EqualTo(n.Transpose().Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = m.EqualTo(m.Transpose().Transpose())
+    AssertEqualHelper(t, true, result)
+
+    m = NewMatrix([][]float64{{0, 1}, {2, 3}, {4, 5}})
+    n = NewMatrix([][]float64{{0, 2, 4}, {1, 3, 5}})
+    result = m.EqualTo(n.Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = n.EqualTo(m.Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = n.EqualTo(n.Transpose().Transpose())
+    AssertEqualHelper(t, true, result)
+
+    result = m.EqualTo(m.Transpose().Transpose())
+    AssertEqualHelper(t, true, result)
+}
+
+func TestEqualTo(t *testing.T) {
+    m := NewMatrix([][]float64{{1, 2, 3}})
+    n := NewMatrix([][]float64{{4, 5, 6}})
+    expected := false
+    result := m.EqualTo(n)
+    AssertEqualHelper(t, expected, result)
+
+    result = n.EqualTo(m)
+    AssertEqualHelper(t, expected, result)
+
+    n = NewMatrix([][]float64{{1, 2, 3}})
+    expected = true
+    result = m.EqualTo(n)
+    AssertEqualHelper(t, expected, result)
+
+    m = NewMatrix([][]float64{{1}, {2}, {3}})
+    n = NewMatrix([][]float64{{4, 5, 6}})
+    expected = false
+    result = m.EqualTo(n)
+    AssertEqualHelper(t, expected, result)
+
+    m = NewMatrix([][]float64{{1, 2}, {3, 4}, {5, 6}})
+    expected = false
+    result = m.EqualTo(n)
+    AssertEqualHelper(t, expected, result)
+
+    n = NewMatrix([][]float64{{1, 2}, {3, 4}, {5, 6}})
+    expected = true
+    result = m.EqualTo(n)
+    AssertEqualHelper(t, expected, result)
+}
+
+func TestAdd(t *testing.T) {
+    m := NewMatrix([][]float64{{1, 2, 3}})
+    n := NewMatrix([][]float64{{4, 5, 6}})
+    expected := NewMatrix([][]float64{{5, 7, 9}})
+    result := m.Add(n)
+    AssertEqualHelper(t, true, expected.EqualTo(result))
+
+    m = NewMatrix([][]float64{{1}, {2}, {3}})
+    n = NewMatrix([][]float64{{4}, {5}, {6}})
+    expected = NewMatrix([][]float64{{5}, {7}, {9}})
+    result = m.Add(n)
+    AssertEqualHelper(t, true, expected.EqualTo(result))
+
+    m = NewMatrix([][]float64{{1, 2}, {2, 3}, {3, 4}})
+    n = NewMatrix([][]float64{{4, 5}, {5, 6}, {6, 7}})
+    expected = NewMatrix([][]float64{{5, 7}, {7, 9}, {9, 11}})
+    result = m.Add(n)
+    AssertEqualHelper(t, true, expected.EqualTo(result))
+}
+
+func TestMultiply(t *testing.T) {
+    m := NewMatrix([][]float64{{1, 2}, {3, 4}})
+    n := NewMatrix([][]float64{{5, 6}, {7, 8}})
+    expected := NewMatrix([][]float64{{19, 22}, {43, 50}})
+    result := m.Multiply(n)
+    AssertEqualHelper(t, true, expected.EqualTo(result))
+}
+
+func TestDot(t *testing.T) {
+    m := NewMatrix([][]float64{{1}, {2}, {3}})
+    n := NewMatrix([][]float64{{4, 5, 6}})
+    expected := 32.0
+    result := m.Dot(n)
+    AssertEqualHelper(t, expected, result)
+
+    m = NewMatrix([][]float64{{4}, {5}, {6}})
+    n = NewMatrix([][]float64{{1, 2, 3}})
+    expected = 32.0
+    result = m.Dot(n)
+    AssertEqualHelper(t, expected, result)
+
+    m = NewMatrix([][]float64{{1}, {2}, {3}})
+    n = NewMatrix([][]float64{{1, 2, 3}})
+    expected = 14.0
+    result = m.Dot(n)
+    AssertEqualHelper(t, expected, result)
+}
